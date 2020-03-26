5186ca4de74d5f7facb440ae5edbf122e858ce42
tutkuince <tutku.ince@outlook.com>
2020-03-26 17:39:48 +0300

    Testing time out

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,5 +34,19 @@ class IndexControllerTest {
         assertThrows(ValueNotFoundException.class,
                 () -> indexController.oopsHandler()
                 );
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOut() {
+        assertTimeout(Duration.ofMillis(100),
+                () -> Thread.sleep(2000));
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOutPreemptively() {
+        assertTimeoutPreemptively(Duration.ofMillis(100),
+                () -> Thread.sleep(2000));
     }
 }
