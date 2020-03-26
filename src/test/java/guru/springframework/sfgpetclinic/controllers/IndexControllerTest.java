fc460399b451a2cee8961b0d0e7b6aab00c24648
tutkuince <tutku.ince@outlook.com>
2020-03-26 14:09:55 +0300

    IndexControllerTest created and some examples with JUnit assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    private IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        indexController = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", indexController.index());
+        assertEquals("index", indexController.index(), "Wrong View Returned!");
+        assertEquals("index", indexController.index(), () -> "This is some expensive " +
+                "Message to build" +
+                "for my test");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(indexController.oupsHandler()), () -> "This is another Expensive Message " +
+                "Make me only if you have to");
+    }
+}
