de4289387866f3abeb3193124ab59b55d0a68a96
tutkuince <tutku.ince@outlook.com>
2020-03-30 12:53:44 +0300

    Conditional JUnit Test Execution

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
 import java.time.Duration;
 
@@ -53,6 +57,7 @@ class IndexControllerTest {
 
 
     @Test
+    @Disabled
     void testAssumptionTrue() {
         assumeTrue("INCE".equalsIgnoreCase(System.getenv("INCE_RUNTIME")));
     }
@@ -60,5 +65,29 @@ class IndexControllerTest {
     @Test
     void testAssumptionIsTrue() {
         assumeTrue("INCE".equalsIgnoreCase("INCE"));
+    }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOs() {
+
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows() {
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11() {
+
     }
 }
